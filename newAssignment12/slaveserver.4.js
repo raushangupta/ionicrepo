e3fa63a35d0a83da2654ed9a53777d2a421d3884
Raushan <raushan@innoventes.com>
2017-09-04 11:35:45 +0530

    added newAssignment to the repo

---
+++ newAssignment12/slaveserver.4.js
@@ -0,0 +1,43 @@
+var http = require('http');
+var express = require('express');
+var app = express();
+var MASTER_HOST = "localhost";
+var MASTER_PORT = 9090;
+
+
+
+app.get('/alive', function (req, res) {
+    res.status(200).send({"message" : 'i am alive'});
+});
+var body =JSON.stringify({
+    hostname: "localhost",
+    portnumber: "9064"
+});
+var server = app.listen(9064, function () {
+    var host = server.address().address;
+    var port = server.address().port;  
+    console.log("slave server listening at http://%s:%s", host, port);
+});
+var options = {
+    hostname: MASTER_HOST,
+    port: MASTER_PORT,
+    path: '/registerme',
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+    }
+};
+var req = http.request(options, (res) => {
+    res.setEncoding('utf8');
+    res.on('data', (chunk) => {
+        console.log(`BODY: ${chunk}`);
+    });
+    res.on('end', () => {
+        console.log('No more data in response.');
+    });
+});
+req.on('error', (e) => {
+    console.log(`problem with request: ${e.message}`);
+});
+req.write(body);
+req.end();
